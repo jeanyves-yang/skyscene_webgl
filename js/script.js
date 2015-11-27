386f525d729743ec7d641f83e0977ade784592ab
Jean-yves Yang <jean-yves.yang@mastertp>
2015-11-27 15:43:30 +0100

    STYLE: cleaning obsolete code

--- js/script.js
+++ js/script.js
@@ -10,9 +10,6 @@ function startGame() {
          scene.enablePhysics(new BABYLON.Vector3(0,-10,0), new BABYLON.OimoJSPlugin());
          
          //define camera
-         /*var camera = new BABYLON.ArcRotateCamera("Camera", 0, Math.PI/6, 13, new BABYLON.Vector3(5,5,0), scene);
-         camera.attachControl(canvas, false);*/
-
          var camera = new BABYLON.ArcRotateCamera("Camera", 0.86, 1.37, 250, BABYLON.Vector3.Zero(), scene);
          camera.attachControl(canvas, false);
          camera.maxZ = 5000;
@@ -32,12 +29,12 @@ function startGame() {
          var skybox = BABYLON.Mesh.CreateSphere("skyBox", 10, 1250, scene);
          meshes.push(skybox);
              //Creation of a repeated textured material
-             var materialSpace = new BABYLON.StandardMaterial("textureSpace", scene);
-             materialSpace.diffuseTexture = new BABYLON.Texture("img/space.jpg", scene);
-             materialSpace.diffuseTexture.uScale = 5.0;//Repeat 5 times on the Vertical Axes
-             materialSpace.diffuseTexture.vScale = 5.0;//Repeat 5 times on the Horizontal Axes
-             materialSpace.backFaceCulling = false;//Hide the back of an element
-             materialSpace.alpha = 1;
+         var materialSpace = new BABYLON.StandardMaterial("textureSpace", scene);
+         materialSpace.diffuseTexture = new BABYLON.Texture("img/space.jpg", scene);
+         materialSpace.diffuseTexture.uScale = 5.0;//Repeat 5 times on the Vertical Axes
+         materialSpace.diffuseTexture.vScale = 5.0;//Repeat 5 times on the Horizontal Axes
+         materialSpace.backFaceCulling = false;//Hide the back of an element
+         materialSpace.alpha = 1;
          var shader = new BABYLON.ShaderMaterial("gradient", scene, "gradient", {});
          shader.setFloat("offset", 0);
          shader.setFloat("exponent", 0.6);
@@ -50,20 +47,6 @@ function startGame() {
 
          var skybox2 = BABYLON.Mesh.CreateSphere("skyBox2", 10, 300, scene);
          meshes.push(skybox2);
-         /*var shader2 = new BABYLON.ShaderMaterial("gradient", scene, "gradient", {});
-         shader2.setFloat("offset", 0);
-         shader2.setFloat("exponent", 0.6);
-         shader2.setColor3("topColor", BABYLON.Color3.FromInts(0,119,255));
-         shader2.setColor3("bottomColor", BABYLON.Color3.FromInts(0,119, 255));
-         shader2.backFaceCulling = false;*/
-
-             //Creation of a repeated textured material
-//             var materialClouds = new BABYLON.StandardMaterial("textureClouds", scene);
-//             materialClouds.diffuseTexture = new BABYLON.Texture("img/ciel_4.jpg", scene);
-//             materialClouds.diffuseTexture.uScale = 1.0;//Repeat 5 times on the Vertical Axes
-//             materialClouds.diffuseTexture.vScale = 1.0;//Repeat 5 times on the Horizontal Axes
-//             materialClouds.backFaceCulling = false;//Always show the front and the back of an element
-//             materialClouds.alpha = 1.0;
          skybox2.material = shader;
 
 
@@ -78,49 +61,6 @@ function startGame() {
          materialMountain.backFaceCulling = true;//Hide the back of an element
          materialMountain.alpha = 1;
          mountain_ground.material = materialMountain;
-
-
-         //define knot object + material
-         /*var knot = BABYLON.Mesh.CreateTorusKnot("mesh", 2, 0.5, 128, 64, 2, 50, scene);
-         meshes.push(knot);
-         knot.position.y = 10;
-         var material = new BABYLON.StandardMaterial("mat", scene);
-         knot.material = material;
-         material.diffuseColor = new BABYLON.Color3(1.5, 0, 0);
-
-         var alpha = 0;
-         knot.scaling.y = 1.5;
-         scene.beforeRender = function() {
-         knot.rotation.y = alpha;
-         alpha += 0.03;
-         };
-
-         knot.setPhysicsState({impostor:BABYLON.PhysicsEngine.SphereImpostor, move:true, mass:1, friction:0.5, restitution:1.0});*/
-         //d.setPhysicsState({impostor:BABYLON.PhysicsEngine.BoxImpostor, move:true, mass:1, friction:0.5, restitution:0.1});
-
-         //define ground
-         /*var precision = {
-         "w" : 2,
-         "h" : 2
-         };
-         var subdivisions = {
-         'h' : 8,
-         'w' : 8
-         };
-         var tiledGround = BABYLON.Mesh.CreateTiledGround("Tiled Ground", -12, -12, 12, 12, subdivisions, precision, scene, false);
-         meshes.push(tiledGround);
-         tiledGround.position.y = -5;
-
-         var ground_material = new BABYLON.StandardMaterial("ground", scene);
-         var t = new BABYLON.Texture("img/ground.jpg", scene);
-         t.uScale = t.vScale = 1;
-         ground_material.diffuseTexture = t;
-         ground_material.specularColor = BABYLON.Color3.Black();
-         tiledGround.material = ground_material;
-
-         tiledGround.setPhysicsState({ impostor: BABYLON.PhysicsEngine.BoxImpostor, move:false});*/
-
-
 
          //render loop (display)
          var renderLoop = function () {scene.render();};
